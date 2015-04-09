b663f2c26ebf8b2ab3302d33f96324b17a8d34d3
catlag <sammieeagle@gmail.com>
2015-04-08 22:29:53 -0700

    finished layouts added ajax calls

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
 // var win_y = $(window).height();
-var win_y = $(window).height();
-
+var winY = $(window).height();
+var nextPageToken;
 
 $(document).ready(function(){
 
-  	var height = win_y - 75;
+	$('#hero').css("height", winY);
+	$.ajax({
+  url: "https://www.googleapis.com/youtube/v3/search?part=snippet"
+  									+"&q=astrology"
+  									+"&maxResults=3"
+                    +"&order=relevance"
+                    +"&type=video"
+                    +"&videoEmbeddable=true"
+                    +"&key=AIzaSyAh38PqfNFaKlVU7gZcFQFFO3hTdrmOdcs",
+  success: function(data){
+  	nextPageToken = data.nextPageToken;
+  	for (var i = 0; i < data.items.length; i++) {
+  		console.log(data.items[i].snippet.title)
+  	};
+  }
+})
+});
 
-  	$('#hero').css("height", height );
+$('#more_videos').click(function(){
+	$.ajax({
+  url: "https://www.googleapis.com/youtube/v3/search?part=snippet"
+  									+"&q=astrology"
+  									+"&maxResults=3"
+                    +"&order=relevance"
+                    +"&type=video"
+                    +"&videoEmbeddable=true"
+                    +"&pageToken="+nextPageToken
+                    +"&key=AIzaSyAh38PqfNFaKlVU7gZcFQFFO3hTdrmOdcs",
+  success: function(data){
+  	nextPageToken = data.nextPageToken
+  	for (var i = 0; i < data.items.length; i++) {
+  		console.log(data.items[i].snippet.title)
+  	};
+  }
+})
 })
