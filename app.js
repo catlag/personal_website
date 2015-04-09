6481f2fca2d61220d97c47cfec423732781cb428
catlag <sammieeagle@gmail.com>
2015-04-08 22:54:28 -0700

    pagination

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // var win_y = $(window).height();
 var winY = $(window).height();
 var nextPageToken;
+var thumbnails
 
 $(document).ready(function(){
 
 	$('#hero').css("height", winY);
 	$.ajax({
   url: "https://www.googleapis.com/youtube/v3/search?part=snippet"
-  									+"&q=astrology"
+  									+"&q=astronomy|planets"
   									+"&maxResults=3"
                     +"&order=relevance"
                     +"&type=video"
@@ -16,7 +17,11 @@ $(document).ready(function(){
   success: function(data){
   	nextPageToken = data.nextPageToken;
   	for (var i = 0; i < data.items.length; i++) {
-  		console.log(data.items[i].snippet.title)
+  		console.log(data.items[i])
+  		thumbnails = data.items[i].snippet.thumbnails.medium.url
+  		var title = data.items[i].snippet.title
+  		$('#results').append("<li><img src='"+thumbnails+"'</img><a>"
+  		 +title+ "</a></li>")
   	};
   }
 })
@@ -25,7 +30,7 @@ $(document).ready(function(){
 $('#more_videos').click(function(){
 	$.ajax({
   url: "https://www.googleapis.com/youtube/v3/search?part=snippet"
-  									+"&q=astrology"
+  									+"&q=astronomy|planets"
   									+"&maxResults=3"
                     +"&order=relevance"
                     +"&type=video"
@@ -35,7 +40,11 @@ $('#more_videos').click(function(){
   success: function(data){
   	nextPageToken = data.nextPageToken
   	for (var i = 0; i < data.items.length; i++) {
-  		console.log(data.items[i].snippet.title)
+  		console.log(data.items[i].snippet.thumbnails.medium.url)
+  		thumbnails = data.items[i].snippet.thumbnails.medium.url
+  		var title = data.items[i].snippet.title
+  				$('#results').append("<li><img src='"+thumbnails+"'</img><a>"
+  		 +title+ "</a></li>")
   	};
   }
 })
