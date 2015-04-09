ef4436965e2f7ccb6a2a781d9b22cb0ef3924f1e
catlag <sammieeagle@gmail.com>
2015-04-08 23:55:23 -0700

    remove list items

--- app.js
+++ app.js
@@ -17,11 +17,11 @@ $(document).ready(function(){
   success: function(data){
   	nextPageToken = data.nextPageToken;
   	for (var i = 0; i < data.items.length; i++) {
-  		// console.log(data.items[i])
+  		vidId = data.items[i].id.videoId
   		thumbnails = data.items[i].snippet.thumbnails.medium.url
   		var title = data.items[i].snippet.title
-  		$('#results').append("<li><img src='"+thumbnails+"'</img><a>"
-  		 +title+ "</a></li>")
+  		$('#results').append("<li><p style='display:none;'>"+vidId+"</p><img src='"+thumbnails+"'</img><a>"
+  		 +title+ "</a><h1>X</h1></li>")
   	};
   	addListener();
   }
@@ -41,28 +41,36 @@ $('#more_videos').click(function(){
 		  success: function(data){
 		  	nextPageToken = data.nextPageToken
 		  	for (var i = 0; i < data.items.length; i++) {
-		  		console.log(data.items[i].snippet.thumbnails.medium.url)
+		  		vidId = data.items[i].id.videoId
 		  		thumbnails = data.items[i].snippet.thumbnails.medium.url
 		  		var title = data.items[i].snippet.title
-		  				$('#results').append("<li><img src='"+thumbnails+"'</img><a>"
-		  		 +title+ "</a></li>")
-		  	};
+		  		$('#results').append("<li><p style='display:none;'>"+vidId+"</p><img src='"+thumbnails+"'</img><a>"
+  		 			+title+ "</a><h1>X</h1></li>")
+  					};
 		  	addListener();
 		  }
 		})
 })
 
 addListener = function(){
+	var video;
 	$('#results').each(function(i,el){
-	$(this).children().children().click(function(){
-		alert("hello")
-	});
-	$(this).children().children().hover(function(){
-      $('html,body').css('cursor','pointer');
-    },
-    function(){
-      $('html,body').css('cursor','auto');
-    })
+		$(this).children().children("img, a").click(function(){
+			video = $(this).siblings("p").html();
+				$('html, body').animate({
+		        scrollTop: $("#personal").offset().top
+		    }, 500);
+				$("iframe").attr("src","https://www.youtube.com/embed/"+video+"?controls=0&amp;showinfo=0")
+		});
+		$(this).children().children("h1").click(function(){
+			$(this).parents("li").remove()
+		});
+		$(this).children().children().hover(function(){
+	      $('html,body').css('cursor','pointer');
+	    },
+	    function(){
+	      $('html,body').css('cursor','auto');
+	    })
 })
 };
 
