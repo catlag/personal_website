9b6423edbf087859921a5d14dd86e81d007bf1a4
catlag <sammieeagle@gmail.com>
2015-05-14 14:19:45 -0500

    added information to projects

--- public/js/script.js
+++ public/js/script.js
@@ -8,91 +8,37 @@ $(document).ready(function(){
 	$('#hero').css("height", winY);
 	$('#columns').css("height", winY);
 	$('#javascript').css("height", winY);
-
-// 	$.ajax({
-//   url: "https://www.googleapis.com/youtube/v3/search?part=snippet"
-//   									+"&q=astronomy|planets"
-//   									+"&maxResults=3"
-//                     +"&order=relevance"
-//                     +"&type=video"
-//                     +"&videoEmbeddable=true"
-//                     +"&key=AIzaSyAh38PqfNFaKlVU7gZcFQFFO3hTdrmOdcs",
-//   success: function(data){
-//   	nextPageToken = data.nextPageToken;
-//   	for (var i = 0; i < data.items.length; i++) {
-//   		vidId = data.items[i].id.videoId
-//   		thumbnails = data.items[i].snippet.thumbnails.medium.url
-//   		var title = data.items[i].snippet.title
-//   		$('#results').append("<li><p style='display:none;'>"+vidId+"</p><img src='"+thumbnails+"'</img><a>"
-//   		 +title+ "</a><h1>X</h1></li>")
-//   	};
-//   	addListener();
-//   }
-// })
-// });
-
-// $('#more_videos').click(function(){
-// 		$.ajax({
-// 		  url: "https://www.googleapis.com/youtube/v3/search?part=snippet"
-// 		  									+"&q=astronomy|planets"
-// 		  									+"&maxResults=3"
-// 		                    +"&order=relevance"
-// 		                    +"&type=video"
-// 		                    +"&videoEmbeddable=true"
-// 		                    +"&pageToken="+nextPageToken
-// 		                    +"&key=AIzaSyAh38PqfNFaKlVU7gZcFQFFO3hTdrmOdcs",
-// 		  success: function(data){
-// 		  	nextPageToken = data.nextPageToken
-// 		  	for (var i = 0; i < data.items.length; i++) {
-// 		  		vidId = data.items[i].id.videoId
-// 		  		thumbnails = data.items[i].snippet.thumbnails.medium.url
-// 		  		var title = data.items[i].snippet.title
-// 		  		$('#results').append("<li><p style='display:none;'>"+vidId+"</p><img src='"+thumbnails+"'</img><a>"
-//   		 			+title+ "</a><h1>X</h1></li>")
-//   					};
-// 		  	addListener();
-// 		  }
-// 		})
-// })
-
-// addListener = function(){
-// 	var video;
-// 	$('#results').each(function(i,el){
-// 		$(this).children().children("img, a").click(function(){
-// 			video = $(this).siblings("p").html();
-// 				$('html, body').animate({
-// 		        scrollTop: $("#personal").offset().top
-// 		    }, 500);
-// 				$("iframe").attr("src","https://www.youtube.com/embed/"+video+"?controls=0&amp;showinfo=0")
-// 		});
-// 		$(this).children().children("h1").click(function(){
-// 			$(this).parents("li").remove()
-// 		});
-// 		$(this).children().children().hover(function(){
-// 	      $('html,body').css('cursor','pointer');
-// 	    },
-// 	    function(){
-// 	      $('html,body').css('cursor','auto');
-// 	    })
-// })
 });
 
+$(".projectImg").hover(function(){
+	$(this).find('p').animate({
+    opacity: 1,
+  }, 1000, function() {
+    console.log("complete")
+  })
+},function(){
+	$(this).find('p').animate({
+    opacity: 0,
+  }, 1000, function() {
+    console.log("complete")
+  })
+}
+);
+
 $('#arrow1').click(function(){
-	alert("clicked!!!");
 	   $('html, body').animate({
         scrollTop: $("#columns").offset().top
     }, 1000);
 	});
 $('#arrow2').click(function(){
-	alert("clicked!!!");
 	   $('html, body').animate({
         scrollTop: $("#javascript").offset().top
     }, 1000);
 	});
 $('#arrow3').click(function(){
-	alert("clicked!!!");
 	   $('html, body').animate({
         scrollTop: $("#bottom").offset().top
     }, 1000);
 	});
-console.log("hello")
+
+
